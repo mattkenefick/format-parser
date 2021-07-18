d5391acf64fd26e947915fa5cbe2e050491d91da
Matt Kenefick <keneficksays@gmail.com>
2021-07-18 15:22:24 -0400

    Add ESLint to UI

--- script/main.js
+++ script/main.js
@@ -1,13 +1,30 @@
 
+import ESLint from './utility/eslint/eslint.js';
 import Prettier from './utility/prettier/prettier.js';
 
+// ┌──────────────────────────────────────────────────────────────────────────────┐
+// │                                                                              │
+// │ Format Generator                                                             │
+// │                                                                              │
+// │ This project / file is intentionally basic for the time being.               │
+// │ The goal is to build out the rules for formatters before wasting             │
+// │ time over-structuring the UI.                                                │
+// │                                                                              │
+// └──────────────────────────────────────────────────────────────────────────────┘
 
-// References
+
+// Setup / References
 // ---------------------------------------------------------------------------
 
 const optFormat = document.querySelector('#optFormat');
-const txtInput = document.querySelector('#txtInput');
-const txtOutput = document.querySelector('#txtOutput');
+const txtESLintOutput = document.querySelector('#txtESLintOutput');
+const txtPrettierOutput = document.querySelector('#txtPrettierOutput');
+const editor = setupEditor();
+
+const eslint = new ESLint({
+    format: 'json',
+});
+
 const prettier = new Prettier({
     format: 'json',
 });
@@ -17,44 +34,62 @@ const prettier = new Prettier({
 // ---------------------------------------------------------------------------
 
 optFormat.addEventListener('click', Handle_OnChangeFormat);
-txtOutput.addEventListener('click', Handle_OnClickOutput);
-
-const editor = ace.edit('txtInput');
-editor.setTheme('ace/theme/tomorrow_night');
-editor.session.setMode('ace/mode/typescript');
-editor.session.on('change', function() {
-    update();
-});
+txtESLintOutput.addEventListener('click', Handle_OnClickOutput);
+txtPrettierOutput.addEventListener('click', Handle_OnClickOutput);
 
 
 // Event Handlers
 // ---------------------------------------------------------------------------
 
+/**
+ * When we change the format dropdown box.
+ *
+ * @param Event e
+ * @return void
+ */
 function Handle_OnChangeFormat(e) {
     e.preventDefault();
 
     const format = e.currentTarget.value;
 
     // Change format
+    eslint.generator.setFormat(format);
     prettier.generator.setFormat(format);
 
     // Update
     update();
 }
 
+/**
+ * Copy our output upon click
+ *
+ * @param Event e
+ * @return void
+ */
 function Handle_OnClickOutput(e) {
     e.preventDefault();
 
     e.currentTarget.select();
-
     document.execCommand('copy');
 }
 
+/**
+ * Process ESLint + Prettier rules
+ *
+ * @return void
+ */
 function update() {
     const input = editor.session.getValue();
-    const output = prettier.parse(input);
+    const prettierOutput = prettier.parse(input);
+    const eslintOutput = eslint.parse(input);
 
-    txtOutput.value = output;
+    // Display for user
+    txtESLintOutput.value = eslintOutput;
+    txtPrettierOutput.value = prettierOutput;
+
+    // Log for developer
+    console.log('ESLint:', eslintOutput);
+    console.log('Prettier:', prettierOutput);
 }
 
 update();
@@ -64,3 +99,19 @@ update();
 // ---------------------------------------------------------------------------
 
 document.body.classList.add('state-active');
+
+
+// Helpers
+// ---------------------------------------------------------------------------
+
+function setupEditor() {
+    const editor = ace.edit('txtInput');
+
+    editor.setTheme('ace/theme/tomorrow_night');
+    editor.session.setMode('ace/mode/typescript');
+    editor.session.on('change', function() {
+        update();
+    });
+
+    return editor;
+}
